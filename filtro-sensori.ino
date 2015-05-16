a9d2b5e5ca782ce8a5cd6633c81cab1738a747f9
Sonny <sonny.zanfardino@gmail.com>
2015-05-16 15:01:44 +0200

    Update filtro-sensori.ino
    Non funziona così...non legge lo stato dei sensori e di conseguenza mi da valori assurdi nel monitor seriale.
    ho modificato qualcosa e adesso leggo e scrivo correttamente o valori del sensore sul motore.
    Ma l'azione di filtraggio non avviene....

--- filtro-sensori.ino
+++ filtro-sensori.ino
@@ -1,19 +1,14 @@
 int previousValue;
 int currentValue;
 Servo myservo; 
-
-int noise = 350;//how much the peak are high 
-
+int noise = 350;//how much the peak are high
 void setup() {
   previousValue = analogRead(0);
   currentValue = previousValue;
   expectedValue = previousValue;
-  
   myservo.attach(9);
-  
   Serial.begin(9600); 
 }
-
 void loop() {
   currentValue = analogRead(0);
   if( abs(currentValue-previousValue) > noise){
