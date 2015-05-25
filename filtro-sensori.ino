42275ec63e9a5332401e8651f1c7a2a3491ce7dd
Sonny <sonny.zanfardino@gmail.com>
2015-05-25 18:14:45 +0200

    Listato per tutte e 5 le dita
    Questo è il listato funzionante con filtro che ho realizzato per far funzionare la mano. 
    
    Può essere ottimizzato?

--- filtro-sensori.ino
+++ filtro-sensori.ino
@@ -1,32 +1,146 @@
-int previousValue; //ultimo misuramento
-int currentValue; //valore corrente
-Servo myservo; 
-int flexSensor=0;
+#include <Servo.h>
 
-int noise = 350;//se il prossimo rilevamento è cambia meno di 350, viene scartato
+Servo myservo1;  
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+Servo myservo5;
 
-void setup() {
-  pinMode(flexSensor, INPUT);
-  previousValue = analogRead(flexSensor);
-  currentValue = previousValue;
-  expectedValue = previousValue;
+int pin1= 1;
+int val1; 
+int v1;
+int curr1;
+int prev1=0;
+unsigned int diff1;                             
+
+int pin2= 2;
+int val2; 
+int v2;
+int curr2;
+int prev2=0;
+unsigned int diff2;                             
+
+int pin3= 3;
+int val3; 
+int v3;
+int curr3;
+int prev3=0;
+unsigned int diff3;                             
+
+int pin4= 0;
+int val4; 
+int v4;
+int curr4;
+int prev4=0;
+unsigned int diff4;                             
+                                              
+int pin5= 0;
+int val5; 
+int v5;
+int curr5;
+int prev5=0;
+unsigned int diff5;                             
+
+void setup()
+{   
+  myservo1.attach(5);                          
+  myservo2.attach(6);                          
+  myservo3.attach(9);                          
+  myservo4.attach(10);                          
+  myservo5.attach(11);                          
+    
+  pinMode(pin1, INPUT);   
+  pinMode(pin2, INPUT);   
+  pinMode(pin3, INPUT);   
+  pinMode(pin4, INPUT);   
+  pinMode(pin5, INPUT);   
   
-  myservo.attach(9);
-  
-  Serial.begin(9600); 
+  Serial.begin(9600);                          
 }
 
-void loop() {
-  currentValue = analogRead(flexSensor);
-  if( abs(currentValue-previousValue) > noise){ //abs è la funzione modulo abs(-5) == 5
-    previousValue=currentValue;  
-  }
-  else{
-    currentValue=previousValue; 
-  } 
-  Serial.print("current value: ");
-  Serial.println(currentValue);
-  currentValue = map(currentValue, 300, 1023, 0, 180);
-  myservo.write(currentValue);
-  delay(100);
+void loop()
+{
+   Serial.flush();    
+   delay(1000); 
+   
+  
+   val1 = analogRead(pin1);
+   v1 = map(val1, 300, 800, 0, 180);           
+
+   val2 = analogRead(pin2);
+   v2 = map(val2, 300, 800, 0, 180); 
+   
+   val3 = analogRead(pin3);
+   v3 = map(val3, 300, 800, 0, 180);   
+   
+   val4 = analogRead(pin4);
+   v4 = map(val4, 300, 800, 0, 180);    
+   
+   val5 = analogRead(pin5);
+   v5 = map(val5, 300, 800, 0, 180);           
+
+   
+   Serial.println(v1);                       
+   curr1 = v1;                                
+   diff1 = prev1 - curr1;              
+
+   Serial.println(v2);                       
+   curr2 = v2;                                
+   diff2 = prev2 - curr2;                      
+
+   Serial.println(v3);                       
+   curr3 = v3;                                
+   diff3 = prev3 - curr3;                      
+
+   Serial.println(v4);                       
+   curr4 = v4;                                
+   diff4 = prev4 - curr4;                      
+
+   Serial.println(v5);                       
+   curr5 = v5;                                
+   diff5 = prev5 - curr5;                      
+
+
+   if ((diff1)>=40)                            
+   {                                          
+     v1 = v1 - 40;                          
+   }
+   if ((diff2)>=40)                            
+   {                                          
+     v2 = v2 - 40;                          
+   }
+   if ((diff3)>=40)                            
+   {                                          
+     v3 = v3 - 40;                          
+   }
+   if ((diff4)>=40)                            
+   {                                          
+     v4 = v4 - 40;                          
+   }
+   if ((diff5)>=40)                            
+   {                                          
+     v5 = v5 - 40;                          
+   }
+
+   
+   myservo1.write(v1);                                     
+   delay(10);
+   prev1=curr1;                   
+
+   myservo2.write(v2);                                     
+   delay(10);
+   prev2=curr2;                              
+
+   myservo3.write(v3);                                     
+   delay(10);
+   prev3=curr3;                              
+
+   myservo4.write(v4);                                     
+   delay(10);
+   prev4=curr4;                              
+
+   myservo5.write(v5);                                     
+   delay(10);
+   prev5=curr5;                              
+   
 }
